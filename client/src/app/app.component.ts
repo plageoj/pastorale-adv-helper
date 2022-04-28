53e9131eb06d8aab3cccd3e6fc97517214ca0781
Masayuki Sugahara <equaaqua@hotmail.com>
2022-04-29 06:24:17 +0900

    add: user interface

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { Auth } from '@angular/fire/auth';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'client';
+  signedIn = false;
+
+  constructor(private auth: Auth) {
+    this.auth.onAuthStateChanged((user) => {
+      this.signedIn = user !== null;
+    });
+  }
 }
--- client/src/app/components/store/detail/detail.component.ts
+++ client/src/app/components/store/detail/detail.component.ts
@@ -1,4 +1,10 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { MapGeocoder } from '@angular/google-maps';
+import { ActivatedRoute } from '@angular/router';
+import { map, mergeMap } from 'rxjs';
+import { Store } from 'src/app/models/store.model';
+import { StoreService } from 'src/app/services/store.service';
 
 @Component({
   selector: 'app-detail',
@@ -6,5 +12,46 @@ import { Component } from '@angular/core';
   styleUrls: ['./detail.component.scss'],
 })
 export class DetailComponent {
-  constructor() {}
+  store?: Store;
+  loading;
+  location?: google.maps.LatLng;
+  placeId = '';
+
+  constructor(
+    private ss: StoreService,
+    private route: ActivatedRoute,
+    private http: HttpClient,
+    private geo: MapGeocoder
+  ) {
+    this.route.paramMap
+      .pipe(
+        map((params) => params.get('id')),
+        mergeMap((id) => this.ss.get(id!))
+      )
+      .subscribe((store) => {
+        this.store = store;
+
+        this.geo
+          .geocode({
+            address: store.address,
+          })
+          .subscribe(({ results }) => {
+            const [result] = results;
+            this.location = result.geometry.location;
+            this.placeId = result.place_id;
+          });
+      });
+
+    if (typeof google === 'undefined') {
+      this.loading = true;
+      this.http
+        .jsonp(
+          'https://maps.googleapis.com/maps/api/js?key=AIzaSyDHN7_-ERRq7cGsVtPn9BwEo1jgSle-UH8',
+          'callback'
+        )
+        .subscribe(() => {
+          this.loading = false;
+        });
+    }
+  }
 }

a1f74f932f05089af5bbe814971887a02ba3286f
Masayuki Sugahara <equaaqua@hotmail.com>
2022-05-07 02:20:29 +0900

    add: modules for testing

--- client/src/app/app.component.spec.ts
+++ client/src/app/app.component.spec.ts
@@ -1,16 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { FirebaseTestingModule } from './testing/firebase-testing.module';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
+      imports: [RouterTestingModule, FirebaseTestingModule],
+      declarations: [AppComponent],
     }).compileComponents();
   });
 
--- client/src/app/components/store/detail/detail.component.spec.ts
+++ client/src/app/components/store/detail/detail.component.spec.ts
@@ -1,5 +1,11 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { RouterTestingModule } from '@angular/router/testing';
+import { StatusIconPipe } from 'src/app/pipes/status-icon.pipe';
+import { FirebaseTestingModule } from 'src/app/testing/firebase-testing.module';
 import { DetailComponent } from './detail.component';
 
 describe('DetailComponent', () => {
@@ -8,9 +14,16 @@ describe('DetailComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ DetailComponent ]
-    })
-    .compileComponents();
+      declarations: [DetailComponent, StatusIconPipe],
+      imports: [
+        FirebaseTestingModule,
+        RouterTestingModule,
+        HttpClientTestingModule,
+        MatListModule,
+        MatIconModule,
+        MatToolbarModule,
+      ],
+    }).compileComponents();
   });
 
   beforeEach(() => {

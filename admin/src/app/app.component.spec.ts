905535e3be92e1ef99d20231aba40dfc5f4e559a
Masayuki Sugahara <equaaqua@hotmail.com>
2022-05-07 20:34:32 +0900

    add: tests

--- admin/src/app/app.component.spec.ts
+++ admin/src/app/app.component.spec.ts
@@ -1,22 +1,30 @@
-import { TestBed } from '@angular/core/testing';
+import { fakeAsync, TestBed, tick } from '@angular/core/testing';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { Router } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { FirebaseTestingModule } from './testing/firebase-testing.module';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule,
+        RouterTestingModule.withRoutes([
+          {
+            path: 'account/login',
+            component: AppComponent,
+          },
+        ]),
         MatToolbarModule,
         MatSidenavModule,
         MatIconModule,
         MatListModule,
         NoopAnimationsModule,
+        FirebaseTestingModule,
       ],
       declarations: [AppComponent],
     }).compileComponents();
@@ -42,5 +50,16 @@ describe('AppComponent', () => {
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
     expect(compiled.querySelector('mat-sidenav-container')).toBeTruthy();
+  });
+
+  it('should logout the user', async () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const router = TestBed.inject(Router);
+    const navigate = spyOn(router, 'navigateByUrl').and.callThrough();
+    fixture.detectChanges();
+
+    await fixture.componentInstance.logout();
+    fixture.detectChanges();
+    expect(navigate).toHaveBeenCalledWith('/account/login');
   });
 });

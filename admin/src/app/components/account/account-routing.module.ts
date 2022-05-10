ae983b65e7d61eea24c6747c7ee5d887f6d7efcd
Masayuki Sugahara <equaaqua@hotmail.com>
2022-05-10 19:51:54 +0900

    add: unauthorized page

--- admin/src/app/components/account/account-routing.module.ts
+++ admin/src/app/components/account/account-routing.module.ts
@@ -1,5 +1,9 @@
 import { NgModule } from '@angular/core';
-import { AuthGuard, redirectLoggedInTo } from '@angular/fire/auth-guard';
+import {
+  AuthGuard,
+  redirectLoggedInTo,
+  redirectUnauthorizedTo,
+} from '@angular/fire/auth-guard';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { UnauthorizedComponent } from './unauthorized/unauthorized.component';
@@ -13,6 +17,8 @@ const routes: Routes = [
   },
   {
     path: 'unauthorized',
+    canActivate: [AuthGuard],
+    data: { authGuardPipe: () => redirectUnauthorizedTo('/account/login') },
     component: UnauthorizedComponent,
   },
 ];

13623d979ec2aa827148a3d3b18d2fb75e6ef1c3
Masayuki Sugahara <equaaqua@hotmail.com>
2022-10-23 12:13:12 +0900

    add: renewal ui

--- admin/src/app/components/settings/settings.module.ts
+++ admin/src/app/components/settings/settings.module.ts
@@ -1,17 +1,22 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatTabsModule } from '@angular/material/tabs';
 import { SettingsRoutingModule } from './settings-routing.module';
 import { SettingsComponent } from './settings.component';
 
-
 @NgModule({
-  declarations: [
-    SettingsComponent
-  ],
+  declarations: [SettingsComponent],
   imports: [
     CommonModule,
-    SettingsRoutingModule
-  ]
+    SettingsRoutingModule,
+    MatButtonModule,
+    MatCardModule,
+    MatStepperModule,
+    MatTabsModule,
+  ],
 })
-export class SettingsModule { }
+export class SettingsModule {}
